f4a3847093fdbadcc33d6f138a9718ddc7aef572
東 顕正 <k3.higashi@ct-net.co.jp>
2026-06-18 14:21:07 +0900

    Fix Pages deployment entry

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/sinanen-customer-reference-mock/",
+  build: {
+    rollupOptions: {
+      input: "app.html",
+    },
+  },
   plugins: [react()],
 });
